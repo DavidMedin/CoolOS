c237373f55d8be106168c32ccc234889ffb10917
David Medin <david@davidmedin.com>
2024-03-10 23:01:54 -0500

    cleanup nix files

--- shell.nix
+++
@@ -1,8 +0,0 @@
-let
-  pkgs = import <nixpkgs> { };
-  zigMaster = pkgs.callPackage ./zigMaster.nix { };
-  zlsMaster = pkgs.callPackage ./zlsMaster.nix { inherit zigMaster; };
-in
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ zigMaster zlsMaster ];
-}
