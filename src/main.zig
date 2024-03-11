271d4e223b6e9509ae3002913f676c66148ba5c0
David Medin <david@davidmedin.com>
2024-03-11 01:01:12 -0500

    getting integer overflow with zig. yay.

--- src/main.zig
+++ src/main.zig
@@ -1,80 +1,140 @@
 // https://wiki.osdev.org/Zig_Bare_Bones
 const std = @import("std");
 
-// const Multiboot2 = packed struct {
-//     magic: u32 = 0xE85250D6,
-//     architecture: u32 = 0,
-//     header_length: u32,
-//     checksum: u32
-// };
+pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, something : ?usize) noreturn {
+    _ = msg;
+    _ = error_return_trace;
+    _ = something;
+    while(true) {
+        @breakpoint();
+    }
+}
 
-// const MultibootTag align(8) = packed struct {
-//     type:u16,
-//     flags:u16,
-//     size:u32
-// };
-// const MultibootFramebuffer = packed struct {
-//     width:u32,
-//     height:u32,
-//     depth:u32
-// };
+const MBI = struct {
+    total_size : u32, // Total size of everything.
+    reserved : u32 // Not for the kernel to touch.
+} ;
 
-// var multiboot_start : void align(4) linksection(".multiboot2") = {};
-// const multiboot_size : u32 = @intFromPtr(&multiboot_end) - @intFromPtr(&multiboot_start);
-// export const multiboot2 align(4) linksection(".multiboot2") = Multiboot2{
-//     .header_length = multiboot_size,
-//     .checksum = -(0xE85250D6 + 0 + multiboot_size)
-// };
+const TagHeader = struct {
+    type : u32,
+    size : u32,
+};
 
-// var mb_info_head align(4) linksection(".multiboot2") = MultibootTag {
-//     .type =  1,
-//     .flags =  1,
-//     .size = &mb_info_end - &mb_info_head
-// };
-// var mb_info_types: u16 align(4) linksection(".multiboot2") = 4;
-// var mb_info_end: void align(4) linksection(".multiboot2") = {};
+const BasicMemInfo = struct { // type=4, size=16
+    header : TagHeader,
+    mem_lower : u32,
+    mem_upper : u32,
+};
 
+const BIOSBootDevice= struct {// type=5, size=20
+     header : TagHeader,
+     biosdev : u32,
+     partition : u32,
+     sub_partition : u32,
+};
 
-// var mb_frame_tag align(4) linksection(".multiboot2") = MultibootTag {
-//     .type =  1,
-//     .flags =  1,
-//     .size = &mb_frame_end - &mb_frame_tag
-// };
-// var mb_frame_data align(4) linksection(".multiboot2") = MultibootFramebuffer{
-//     .width = 800,
-//     .height = 800,
-//     .depth = 2
-// };
-// var mb_frame_end: void align(4) linksection(".multiboot2") = {};
+const BootCmdLine = struct { // type=1
+    header : TagHeader,
+    start_of_string : u8,
+};
 
-// var mb_end_tag align(4) linksection(".multiboot2") = MultibootTag {
-//     .type = 0,
-//     .flags = 0,
-//     .size = 8
-// };
+const Modules = struct { // type=3
+    header : TagHeader,
+    mod_start : u32,
+    mod_end : u32,
+    string_start : u8,
+};
 
-// const multiboot_end : void align(4) linksection(".multiboot2") = {};
+// TODO: Elf-Symbols
+// TODO: Memory map
 
-// export var _start linksection(".text") = fn callconv("naked") 
-// export fn _start () callconv(.Naked) noreturn {
+const BootLoaderName = struct { // type=2
+     header : TagHeader,
+     string_start : u8,
+} ;
 
-// }
+// TODO: APM
 
-pub export fn kernel_main() callconv(.C) void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const FrameBufferInfo = struct {
+    header : TagHeader,
+    fmbuff_addr : u64,
+    fmbuff_pitch : u32,
+    fmbuff_width : u32,
+    fmbuff_height : u32,
+    fmbuff_bpp : u8,
+    fmbuff_type : u8,
+    reserved : u8,
+    color_info_start : u8, // not actual data, use this address for palette maybe.
+};
 
-    // // stdout is for the actual output of your application, for example if you
-    // // are implementing gzip, then only the compressed bytes should be sent to
-    // // stdout, not any debugging messages.
-    // const stdout_file = std.io.getStdOut().writer();
-    // var bw = std.io.bufferedWriter(stdout_file);
-    // const stdout = bw.writer();
+// =============== Either =======
+const PaletteInfo = struct {
+    fmbuff_palette_num_colors : u32,
+    palette_start : u8,
+};
 
-    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+const palette = struct {
+    red : u8,
+    gree : u8,
+    blue : u8,
+};
 
-    // try bw.flush(); // don't forget to flush!
+// ================ Or ==========
+ var fmbuff_red_field_position : u8 = 0;
+ var fmbuff_red_mask_size : u8 = 0;
+ var fmbuff_green_field_position : u8 = 0;
+ var fmbuff_green_mask_size : u8 = 0;
+ var fmbuff_blue_field_position : u8 = 0;
+ var fmbuff_blue_mask_size : u8 = 0;
 
+// =============================
+
+const ImageLoadBaseAddress = struct {
+    header : TagHeader,
+    base_address : u32,
+};
+
+fn align_to(input : *usize, alignment : u32) void {
+    input.* += ( (alignment - input.*) % alignment);
+}
+
+var base_address : u32 = 0; // bad.
+var MBI_info = [_]u32{0} ** 3000; // Also bad.
+var MBI_end : u32 = 0;
+
+pub export fn kernel_main(mbi : *MBI) callconv(.C) void {
+    var tag_head : *TagHeader = @ptrFromInt( @intFromPtr(mbi) + @sizeOf(MBI) );
+    var tag_addr : u32 = @intFromPtr(tag_head);
+
+    align_to(&tag_addr, 8);
+    var fb_info : *FrameBufferInfo = undefined;
+
+    while(tag_head.*.type != 0) {
+        MBI_info[MBI_end] = tag_head.*.type;
+        MBI_end += 1;
+
+        switch(tag_head.*.type){
+            8 => {
+                fb_info = @ptrCast( tag_head );
+            },
+            else => {
+
+            }
+        }
+
+        tag_addr += tag_head.*.size;
+        align_to(&tag_addr, 8);
+        tag_head = @ptrFromInt(tag_addr);
+    }
+
+    if(fb_info.*.fmbuff_type == 0 ){
+        // color_info is defined by an indexed palette.
+        //fb_info.*.fmbuff_addr = 1;
+    }else if(fb_info.*.fmbuff_type == 1) {
+        const to_usize : usize = @intCast( fb_info.*.fmbuff_addr );
+        const bad_ptr : *u32 = @ptrFromInt( to_usize ) ;
+        bad_ptr.* = std.math.maxInt(u32);
+    }
     var thing : i32 = 3;
     thing = 2;
 }
